a7d68c661de2daf3316caf81e3315fc2da5326c9
aumitleon <aumitleon1@gmail.com>
2018-12-11 21:49:42 -0500

    update server code

--- src/predictions/server/run_server.py
+++ src/predictions/server/run_server.py
@@ -9,36 +9,39 @@ application = Flask(__name__)
 logger = logging.getLogger(__name__)
 logging.basicConfig(format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
 
-
+# Load the model 
 def build_model(model_path):
     with open(model_path, "rb") as f:
         mdl = dill.load(f)
     return mdl
 
-
-def launch_model(mdl, request):
+# Produce a prediction
+def predict(mdl, request):
     try:
         features_as_dict = request.get_json()
         print(features_as_dict)
         features_as_df = pd.io.json.json_normalize(features_as_dict)
 
-        logger.info("Predicting {}".format(features_as_df))
-        print("Preparing to produce")
+        #logger.info("Predicting {}".format(features_as_df))
+        print("Predict using the following features")
+        print(features_as_dict)
+        print("Preparing to predict")
         binary_predictions = mdl.predict(features_as_df)
 
-        detected_zones = str(binary_predictions[0]) + '\n'
-        logger.info("Predicted {}".format(detected_zones))
+        prediction = str(binary_predictions[0]) + '\n'
+        print("Model predicts: " + prediction)
+        #logger.info("Predicted {}".format(prediction))
     except Exception as e:
         logger.exception("Error in pipeline")
         raise BadRequest(description=getattr(e, 'message', repr(e)))
 
-    return detected_zones
+    return prediction
 
 
 @application.route("/predict", methods=["POST"])
 def handle_predict_request():
     global model
-    return launch_model(model, request)
+    return predict(model, request)
 
 
 if __name__ == "__main__":
